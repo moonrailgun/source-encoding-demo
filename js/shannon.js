a3ebd34d370b82575d5979d7e316ce5d545ceec1
moonrailgun <moonrailgun@gmail.com>
2016-01-09 19:34:01 +0800

    香农编码前三步
    ADD 香农编码前三步

--- js/shannon.js
+++ js/shannon.js
@@ -2,6 +2,26 @@
  * Created by Chen on 2016-01-08.
  */
 
-var Shannon = function(source){
+var Shannon = function(_source){
+    var Pa = [];
+    var source = _source;
 
+    this.Analyze = function(){
+        //step 2
+        Pa[0] = 0;
+        for(var i = 0;i<source.length;i++){
+            Pa[i+1] = 0;
+            for(var j = 0;j<Pa.length;j++){
+                if(j==0){
+                    Pa[i+1] += 0;
+                }else{
+                    Pa[i+1] += Number(source[j-1]);
+                }
+            }
+        }
+        console.log('Pa:' + Pa + '|length:' + Pa.length);
+
+        //step 3
+        //todo
+    }
 };
