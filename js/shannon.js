463479f0b8210b3c4c1334e5f35542fc5557f834
moonrailgun <moonrailgun@gmail.com>
2016-01-10 16:20:29 +0800

    香农编码
    ADD 香农编码

--- js/shannon.js
+++ js/shannon.js
@@ -2,26 +2,56 @@
  * Created by Chen on 2016-01-08.
  */
 
-var Shannon = function(_source){
+var Shannon = function (_source) {
     var Pa = [];
-    var source = _source;
+    var Ki = [];
+    var P = [];
+    var Code = [];
 
-    this.Analyze = function(){
+    this.Analyze = function () {
         //step 2
-        Pa[0] = 0;
-        for(var i = 0;i<source.length;i++){
-            Pa[i+1] = 0;
-            for(var j = 0;j<Pa.length;j++){
-                if(j==0){
-                    Pa[i+1] += 0;
-                }else{
-                    Pa[i+1] += Number(source[j-1]);
-                }
+        for (var i = 1; i < P.length; i++) {
+            Pa[i] = 0;
+            for (var j = 0; j < i; j++) {
+                Pa[i] += Number(P[j]);
             }
         }
-        console.log('Pa:' + Pa + '|length:' + Pa.length);
 
         //step 3
-        //todo
-    }
+        for (i = 1; i < Pa.length; i++) {
+            //Ki[i] = Math.floor(-Math.log(P[i]) / Math.log(2));
+            var tmp = -(Math.log(P[i]) / Math.log(2));
+            if (parseInt(tmp) == tmp) {
+                //是整数
+                Ki[i] = tmp;
+            } else {
+                //是浮点数
+                Ki[i] = Math.ceil(tmp);
+            }
+        }
+
+        //step 4
+        for (i = 1; i < Pa.length; i++) {
+            Code[i] = parseInt(Number(Number((Pa[i]).toString(2))) * Math.pow(10, Ki[i]));
+        }
+
+        console.log(Code);
+    };
+
+    this.getAverageCodeLength = function () {
+        var codeLength = 0;
+        for (var i = 1; i < Ki.length; i++) {
+            codeLength += P[i] * Ki[i];
+        }
+
+        return codeLength;
+    };
+
+    (function () {
+        //输入编码
+        P[0] = 0;
+        for (var i = 0; i < _source.length; i++) {
+            P[i + 1] = _source[i];
+        }
+    })();
 };
